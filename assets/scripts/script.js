9cb0f0beca496e81013045f4ffa1b328f0598e9a
codeDevLogan <logandevscode@gmail.com>
2023-01-01 16:32:45 -0700

    Saving work from New Year's Weekend. Next few things: Double and triple check the assignment on bootcampspot to make sure this fulfills all of the asks and finish commenting for future contributions.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,6 +4,7 @@ const lowerCaseArray = 'abcdefghijklmnopqrstuvwxyz'.split('');
 const upperCaseArray = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 const numericArray = '1234567890'.split('');
 const specArray = '!@#$%^&*()_+-={}[]:;<>,.?/~'.split('');
+let boolArray = [false, false, false, false];
 
 let passLength;
 let hasLowerCase;
@@ -12,6 +13,9 @@ let hasNum;
 let hasSpecCase;
 let numOfTypes = 0;
 
+/* getYorN
+
+*/
 function getYorN(msg){
   let YorN = confirm(msg);
   numOfTypes++;
@@ -31,13 +35,46 @@ function getRandom(lowerLim, upperLim){
   }
   return randomNum[2];
 }
+/* isValidPasswordLength
+Use: This function checks the requested passwordLength value to ensure it does not exceed the upper limit and does not fall below the lower limit. It also assures that the length is a number.
+Use with: isValidPassLength(passToCheck);
+passToCheck: This is the variable recieved from the call statement that is used to check against the upper and lower limits. As well as the character type assurances.
+*/
 function isValidPassLength(passToCheck){
+  for(let i=0;i<passToCheck.length; i++){
+    for(let j=0;j<upperCaseArray.length; j++){
+      if(passToCheck[i] == upperCaseArray[j]){
+        alert("Invalid Password Length please try again.");
+        return false;
+      }
+    }
+  }
+  for(let i=0;i<passToCheck.length; i++){
+    for(let j=0;j<lowerCaseArray.length; j++){
+      if(passToCheck[i] == lowerCaseArray[j]){
+        alert("Invalid Password Length please try again.");
+        return false;
+      }
+    }
+  }
+  for(let i=0;i<passToCheck.length; i++){
+    for(let j=0;j<specArray.length; j++){
+      if(passToCheck[i] == specArray[j]){
+        alert("Invalid Password Length please try again.");
+        return false;
+      }
+    }
+  }
   if(passToCheck < 8 || passToCheck > 128){
     alert("Invalid Password Length please try again.");
-    generatePassword();
+    return false;
   }
   return true;
 }
+/* getUserAnswers
+Use: This function collects data from the user to be stored in the global data points. For best user experience this should only be called once inside the generatePassword function.
+Call with: getUserAnswers();
+*/
 function getUserAnswers(){
   hasLowerCase = getYorN("Would you like the password to include: Lower Case Characters?");
   hasUpperCase = getYorN("Would you like the password to include: Upper Case Characters?");
@@ -57,43 +94,47 @@ function confirmUserAnsweredNonNull(){
 }
 /* hasThatType
 Use: This function checks through a given string against the given character type's array to ensure the given string contains at least one instance of that character type.
-Call with: hasThatType(varToCheck, typeToCheck)
+Call with: hasThatType(varToCheck, typeToCheck);
 varToCheck: This is the given string that is being looked through for the character type.
 typeToCheck: This is the given character type indicator.
 */
 function hasThatType(varToCheck, typeToCheck){
   let arrayToCheck = varToCheck.split('');
-  if(typeToCheck==1){
+  if(typeToCheck == 1 && !(boolArray[0])){
     for(let i=0; i<arrayToCheck.length;i++){
       for(let j=0; j<lowerCaseArray.length; j++){
         if(arrayToCheck[i] == lowerCaseArray[j]){
+          boolArray[0] = true;
           return true;
         }
       }
     }
   }
-  if(typeToCheck==2){
+  if(typeToCheck == 2 && !(boolArray[1])){
     for(let i=0; i<arrayToCheck.length;i++){
       for(let j=0; j<upperCaseArray.length; j++){
         if(arrayToCheck[i] == upperCaseArray[j]){
+          boolArray[1] = true;
           return true;
         }
       }
     }
   }
-  if(typeToCheck==3){
+  if(typeToCheck == 3 && !(boolArray[2])){
     for(let i=0; i<arrayToCheck.length;i++){
       for(let j=0; j<numericArray.length; j++){
         if(arrayToCheck[i] == numericArray[j]){
+          boolArray[2] = true;
           return true;
         }
       }
     }
   }
-  if(typeToCheck==4){
+  if(typeToCheck == 4 && !(boolArray[3])){
     for(let i=0; i<arrayToCheck.length;i++){
       for(let j=0; j<specArray.length; j++){
         if(arrayToCheck[i] == specArray[j]){
+          boolArray[3] = true;
           return true;
         }
       }
@@ -102,13 +143,19 @@ function hasThatType(varToCheck, typeToCheck){
   return false;
 }
 
+/* generatePassword
+Use: This function acts as the main through put of the program. The majority of functions are run here and this handles the logic of the button.
+Call with: generatePassword();
+*/
 function generatePassword(){
   let passToReturn = "";
   let randomNumForCharacterGen = getRandom(1, 4);
   console.log("Generate Password");
   console.log(randomNumForCharacterGen);
-  passLength = window.prompt("How long would you like the password to be?");
-  isValidPassLength(passLength);
+  passLength = window.prompt("How long would you like the password to be? (Between 8 and 128 characters in length)");
+  while(!isValidPassLength(passLength)){
+    passLength = window.prompt("How long would you like the password to be? (Between 8 and 128 characters in length)");
+  }
   getUserAnswers();
   if(confirmUserAnsweredNonNull()){
     for(let i=0;i<passLength;i++){
@@ -123,51 +170,51 @@ function generatePassword(){
       if(hasLowerCase){
         //console.log("Has Lower Case");
         passToReturn = passToReturn + lowerCaseArray[getRandom(0, (lowerCaseArray.length-1))];
-        console.log(hasThatType(passToReturn, randomNumForCharacterGen));
+        //console.log(hasThatType(passToReturn, randomNumForCharacterGen));
         if(hasThatType(passToReturn, randomNumForCharacterGen)){
           numOfTypes--;
         }
       }
     }
     if(randomNumForCharacterGen == 2){
-      console.log("Random Number Check");
+      //console.log("Random Number Check");
       if(hasUpperCase){
         //console.log("Has Upper Case");
         passToReturn = passToReturn + upperCaseArray[getRandom(0, (upperCaseArray.length-1))];
-        if(!hasThatType(passToReturn, randomNumForCharacterGen)){
+        if(hasThatType(passToReturn, randomNumForCharacterGen)){
           numOfTypes--;
         }
       }
     }
     if(randomNumForCharacterGen == 3){
-      console.log("Random Number Check");
+      //console.log("Random Number Check");
       if(hasNum){
         //console.log("Has Numbers");
         passToReturn = passToReturn + numericArray[getRandom(0, (numericArray.length-1))];
-        if(!hasThatType(passToReturn, randomNumForCharacterGen)){
+        if(hasThatType(passToReturn, randomNumForCharacterGen)){
           numOfTypes--;
         }
       }
     }
     if(randomNumForCharacterGen == 4){
-      console.log("Random Number Check");
+      //console.log("Random Number Check");
       if(hasSpecCase){
         //console.log("Has Spec Case");
         passToReturn = passToReturn + specArray[getRandom(0, (specArray.length-1))];
-        if(!hasThatType(passToReturn, randomNumForCharacterGen)){
+        if(hasThatType(passToReturn, randomNumForCharacterGen)){
           numOfTypes--;
         }
       }
     }
     randomNumForCharacterGen = getRandom(1, 4);
+    //console.log(numOfTypes);
+    //console.log(randomNumForCharacterGen);
     // The point of this if statement is to ensure the password generated will contain each of the requested character types at least once.
-    console.log(numOfTypes);
-    console.log(randomNumForCharacterGen);
     if(passToReturn.length === passLength-1 && numOfTypes <= 0){
       passToReturn = '';
     }
   }
-  console.log(passToReturn);
+  //console.log(passToReturn);
   return passToReturn;
 }
 
